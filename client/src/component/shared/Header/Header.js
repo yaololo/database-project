2598e51bed7fbd160fc00a8bd6a82dd02a94e66d
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-09-05 20:40:58 +0800

    update setup

---
+++ client/src/component/shared/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div class="panel">
+            <h1>Database-Application</h1>
+        </div>
+    );
+}
+
+export default Header;
+
