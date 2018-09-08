93ef4bcd24f47896d03ea572c48c0e8e21e836e3
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-09-08 17:15:36 +0800

    add remove server and  routes

--- server/app.js
+++ server/app.js
@@ -1,29 +1,22 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
   cors = require("cors");
-const router = require("./routes");
 const path = require("path");
 
 // Create global app object
 const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
+}
 
 app.use(cors());
 
 // Normal express config defaults
 app.use(bodyParser.json());
 
-const staticFiles = express.static(path.join(__dirname, "../client/build"));
+// app.use(express.static(path.join(__dirname, "../client")));
 
-if (process.env.isProduction) {
-  app.use(staticFiles);
-} else {
-  app.use(express.static(path.join(__dirname, "client")));
-}
-
-if (process.env.isProduction) {
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
-}
-
-module.exports = app;
+const server = app.listen(3001, function() {
+  console.log(`Server Listening on port ${server.address().port}...`);
+});
