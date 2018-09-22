5dc3fdf2a94f4895a9f429544ed443dffd95408f
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-09-22 21:06:11 +0800

    Add react router

--- client/src/component/shared/Header/Header.js
+++ client/src/component/shared/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Header.css'
+
 const Header = () => {
-    return (
-        <div class="panel">
-            <h1>Database-Application</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Database-Application</h1>
+    </div>
+  );
+};
 
 export default Header;
-
