8fc6ba7ad879a68e705aab1515fc044c20217251
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-09-22 21:02:42 +0800

    Render Menu and Header with css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from "./component/shared/Header/Header";
+import Header from './component/shared/Header/Header';
+import { BrowserRouter } from 'react-router-dom';
+import Menu from './component/shared/Menu/Menu';
 
 class App extends Component {
   render() {
     return (
-      <Header/> 
+      <div>
+        <div className="big_wrapper">
+          <div className="container-fluid header">
+            <Header />
+          </div>
+          <BrowserRouter>
+            <Menu />
+          </BrowserRouter>
+        </div>
+      </div>
     );
   }
 }
