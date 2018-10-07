331572a85050b8de87cd2158b6955522b4f3a89a
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-07 20:36:04 +0800

    Create footer and header

--- client/src/component/shared/Header/Header.js
+++ client/src/component/shared/Header/Header.js
@@ -1,11 +1,56 @@
-import React from 'react';
-import './Header.css';
-const Header = () => {
-  return (
-    <div className="header">
-      <h1>Database-Application</h1>
-    </div>
-  );
-};
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+class Header extends React.Component {
+  render() {
+    const active = { borderBottomColor: "#3f51b5" };
+    const rightNav =
+      (<ul className="nav navbar-nav navbar-right">
+        <li>
+          <NavLink to="/login" activeStyle={active}>
+            Log in
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup" activeStyle={active}>
+            Sign up
+          </NavLink>
+        </li>
+      </ul>
+    );
+    return (
+      <nav className="navbar navbar-default navbar-static-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbar"
+              className="navbar-toggle collapsed"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <NavLink exact to="/" className="navbar-brand">
+              Project name
+            </NavLink>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li>
+                <NavLink exact to="/" activeStyle={active}>
+                  Home
+                </NavLink>
+              </li>
+            </ul>
+            {rightNav}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
