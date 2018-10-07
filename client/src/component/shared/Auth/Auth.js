aaf63a893676c0dbbba34205c43768d6e54efe07
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-07 21:14:16 +0800

    Create Auth helper functions

---
+++ client/src/component/shared/Auth/Auth.js
@@ -0,0 +1,58 @@
+export function login({
+  email,
+  password,
+  history,
+  from,
+}) {
+  return fetch("/api/users/login", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      user: {
+        email: email,
+        password: password
+      }
+    })
+  }).then(response => {
+    if (response.ok) {
+      return response.json().then(json => {
+
+        history.replace(from);
+      });
+    } else {
+      return response.json().then(json => {
+        console.log('something wrong during login')
+      });
+    }
+  });
+}
+
+
+
+export function signup({
+  name,
+  email,
+  password,
+  history,
+}) {
+  return fetch('/api/users/signup', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      user: {
+        name: name,
+        email: email,
+        password: password
+      }
+    })
+  }).then(response => {
+    return response.json().then(json => {
+      if (response.ok) {
+      } else {
+        console.log('something wrong during signup')
+        // const messages = Array.isArray(json) ? json : [json];
+      }
+    });
+  });
+}
+
