5fcc5b4ece573cafbe4b7aafff7ea2ce4906c4da
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-12 19:37:36 +0800

    Updata signup api handler

--- server/routes/actions/signUpHandler.js
+++ server/routes/actions/signUpHandler.js
@@ -2,30 +2,35 @@ const dbSetup = require('../../DbConnection/setupConnection');
 
 const signUpHandler = async function(req, res) {
   try {
-    console.log(req.body);
-    return res.status(200).send(
-      JSON.stringify({
-        data: req.body.user
-      })
-    );
-    // var connection = dbSetup.connect();
-    // connection.query('SELECT * FROM product', function(error, results, fields) {
-    //   if (error) {
-    //     console.log(error);
-    //     return res.status(500).send(
-    //       JSON.stringify({
-    //         data: 'Something went wrong while retrieving data.'
-    //       })
-    //     );
-    //   } else {
-    //     return res.status(200).send(
-    //       JSON.stringify({
-    //         data: results
-    //       })
-    //     );
-    //   }
-    // });
-    // connection.end();
+    postValue ={
+      user_id: '0000012349',
+      frist_name: req.body.user.firstName,
+      last_name: req.body.user.lastName,
+      email: req.body.user.email,
+      auth: req.body.user.password,
+      user_type: 'customer',
+      gender: req.body.user.gender
+    }
+
+    var connection = dbSetup.connect();
+    connection.query(`INSERT INTO users SET ?`, postValue, function(error, results, fields) {
+      if (error) {
+        console.log(error);
+        return res.status(500).send(
+          JSON.stringify({
+            data: 'Something went wrong while retrieving data.'
+          })
+        );
+      } else {
+        console.log('successful');
+        return res.status(200).send(
+          JSON.stringify({
+            token: 'true'
+          })
+        );
+      }
+    });
+    connection.end();
   } catch (error) {
     console.log(error);
   }
