3b342290d72c9f012c757d9df904e6c295a44116
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-13 13:07:39 +0800

    Add more routes

--- server/routes/api/allRoutes.js
+++ server/routes/api/allRoutes.js
@@ -1,10 +1,18 @@
 const loginHandler = require('../actions/loginHandler');
 const signUpHandler = require('../actions/signUpHandler');
 const onLandingAction = require('../actions/onLanding');
+// const updateCartHandler = require ('../actions/updateCartHandler')
 const router = require('express').Router();
 
 router.get('/hotItems', onLandingAction);
+
 router.post('/user/signup', signUpHandler);
+
 router.post('/user/login', loginHandler);
+
+// router.post('/updateCart', updateCartHandler);
+
+// router.post('/getItemDetails', itemDetailsHandler);
+
 
 module.exports = router;
