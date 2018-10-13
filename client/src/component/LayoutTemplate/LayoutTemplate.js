6829abdf80d01533eae86e64c71bc9665cbd3984
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-13 20:08:17 +0800

    Update layout template to redirect to product details page

--- client/src/component/LayoutTemplate/LayoutTemplate.js
+++ client/src/component/LayoutTemplate/LayoutTemplate.js
@@ -1,29 +1,66 @@
 import React, { Component } from 'react';
 import './LayoutTemplate.css';
+import { mapItemsToCart, mapSessionContextToProps, sessionContextPropType } from '../../context_helper';
+import { ProviderContext, subscribe } from "react-contextual";
+import { withRouter } from 'react-router-dom'
+
 class LayoutTemplate extends Component {
+  static propTypes = {
+    ...sessionContextPropType
+  };
   constructor(props) {
     super()
     this.state = {
       data: props.data
     };
   }
+
+  // addToCartHandler(event){
+  //   event.preventDefault();
+
+  //   updateCart(event.target.value, this.props.sessionContext.user.id)
+  //   console.log(JSON.stringify(event.target.value))
+  //   // this.props.itemsId.push(event.target.value)
+  //   this.props.itemsWithQty = mapArrayToObject(this.props.itemsId)
+  // }
+
+  onClickHandler(item, event){
+    console.log(item)
+    this.props.history.push({
+      pathname:`/product/${item.product_id}`,
+      state:{
+          key:item
+       }
+     });
+  }
+
   render() {
     if (this.props.data !== undefined ) {
       return (
         <div className="container">
         <div className="product-list">
           {this.props.data.map((element, i) => {
+            this.item =element;
+            // this.props.singleItem = element;
             return (
               <div key={i} className="content">
                 <div className="picture">
                   <img src={element.image.split(',')[0]} alt="" />
                 </div>
                 <div className="short-description">
-                  {`$${element.price}`}
+                  {element.p_name}
                 </div>
-                <div className="price">{element.shortDescription}</div>
+                <div className="price">{element.description}</div>
+                <div className="short-description">
+                  {`$${element.unit_price}`}
+                </div>
                 <div className="purchase-button">
-                  <button className="order-button" type="button">
+                  <button 
+                    className="order-button" 
+                    type="button" 
+                    value = {element.product_id}
+                    onClick= {this.onClickHandler.bind(this, this.item)}
+                    >
                     Add to cart
                   </button>
                 </div>
@@ -47,5 +84,11 @@ class LayoutTemplate extends Component {
     }
   }
 }
+const mapCartToProps = context => {
+  return {
+    ...mapItemsToCart(context),
+    ...mapSessionContextToProps(context)
+  };
+};
 
-export default LayoutTemplate;
+export default subscribe( ProviderContext, mapCartToProps ) ( withRouter(LayoutTemplate) );
