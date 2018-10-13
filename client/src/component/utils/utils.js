1232ef9047a2051877d6544f70925af0578dde0c
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-14 00:36:21 +0800

    Update utils updateCart response method

--- client/src/component/utils/utils.js
+++ client/src/component/utils/utils.js
@@ -1,18 +1,15 @@
-export function updateCart (itemId, customerId){
+export function updateCart (itemId, customerId, quantity){
   fetch('/api/updateCart', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       itemId: itemId,
       customerId: customerId,
+      quantity: quantity
     })
   }).then(response => {
     if (response.ok) {
-      response.json().then(json => {
-        this.setState({
-          data: json.data
-        });
-      });
+      console.log('successfully add to cart');
     } else {
       console.log('something wrong during updating cart table');
     }
