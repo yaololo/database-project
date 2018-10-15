c1a64ccd76e4d89b4477676c1b4b2d3bbbf5bc3f
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-15 18:36:52 +0800

    Add error handler

--- server/routes/api/allRoutes.js
+++ server/routes/api/allRoutes.js
@@ -2,15 +2,16 @@ const loginHandler = require('../actions/loginHandler');
 const signUpHandler = require('../actions/signUpHandler');
 const onLandingAction = require('../actions/onLanding');
 const updateCartHandler = require ('../actions/updateCartHandler')
+const handleAsyncError = require("express-async-wrap");
 const router = require('express').Router();
 
-router.get('/hot_items', onLandingAction);
+router.get('/hot_items', handleAsyncError(onLandingAction));
 
-router.post('/user/signup', signUpHandler);
+router.post('/user/signup', handleAsyncError(signUpHandler));
 
-router.post('/user/login', loginHandler);
+router.post('/user/login', handleAsyncError(loginHandler));
 
-router.post('/update_cart', updateCartHandler);
+router.post('/update_cart', handleAsyncError(updateCartHandler));
 
 // router.get('/my_cart_details', cartDetailsHandler);
 
