e66e4099bff269c0b56beac9497e1b7ffdadcf00
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-17 07:54:43 +0800

    Modiy login process and returned values

--- server/routes/actions/loginHandler.js
+++ server/routes/actions/loginHandler.js
@@ -7,21 +7,28 @@ const loginHandler = async function(req, res) {
       email: req.body.user.email,
       password: req.body.user.password.toString(),
     }
-    let cartItemInfo = [];
-    let productList = [];
   
     var connection = dbSetup.connect();
     connection.query('SELECT user_id, first_name, last_name, email, user_type FROM `users` WHERE `email`= ? AND `auth` = ?;', [user.email, user.password], function(error, results, fields) {
       if (error) {
-        console.log('first query something wrong')
+        console.log(error,sqlMessage)
         connection.on('error', function() {} );
         connection.end();
         return res.status(500).send(
           JSON.stringify({
-            msg: 'Invalid email or password.'
+            msg: 'Something wrong during login.'
           })
         );
       }else{
+
+        if(results.length === 0){
+          connection.end();
+          return res.status(500).send(
+            JSON.stringify({
+              msg: "Invalid email or password"
+            })
+          )
+        }
 
         user.email = results[0].email;
         user.firstName = results[0].first_name;
@@ -29,13 +36,13 @@ const loginHandler = async function(req, res) {
         user.userType =  results[0].user_type;
         user.id = results[0].user_id;
 
-        let connection = dbSetup.connect();
+        // let connection = dbSetup.connect();
         
         connection.query('SELECT product_id, SUM(quantity) FROM cart WHERE customer_id = ? GROUP BY product_id', user.id,
         function(error, results, fields) {
           if(error){
-            // console.log(error)
-            connection.on('error', function() {} );
+            console.log(error.sqlMessage)
+            // connection.on('error', function() {} );
             connection.end()
             return res.status(500).send(
               JSON.stringify({
@@ -43,38 +50,29 @@ const loginHandler = async function(req, res) {
               })
             );
           }else{
-            cartItemInfo = results;
-            // console.log(cartItemInfo)
-            let count = 0;
-            for(i=0 ; i< cartItemInfo.length; i++){
-              let connection = dbSetup.connect();
-              connection.query('SELECT * FROM product WHERE product_id = ?', cartItemInfo[i].product_id,
-              function(error, results, fields) {
-                if(error){
-                  console.log(error.sqlMessage)
-                  connection.on('error', function() {} );
-                  connection.end();
-                  return res.status(500).send(
-                    JSON.stringify({
-                      msg: "Get shopping cart item fail"
-                    })
-                  );
-                } else{
-                  results[0].quantity = cartItemInfo[count]['SUM(quantity)']
-                  productList.push(results);
-                  count ++;
-                  if(productList.length === cartItemInfo.length){
-                    return res.status(200).send(
-                      JSON.stringify({
-                        token: "true",
-                        user: user,
-                        productList: productList
-                      })
-                    );
-                  }
-                }
-              })
+            if(results.length === 0){
+              return res.status(200).send(
+                JSON.stringify({
+                  msg: "login successful",
+                  token: "true",
+                  user: user,
+                  productList: []
+                })
+              )
             }
+            
+            let productList = {
+              product_id: results[0],
+              quantity: result[0]['SUM(quantity)']
+            };
+            return res.status(200).send(
+              JSON.stringify({
+                msg: "login successful",
+                token: "true",
+                user: user,
+                productList: productList
+              })
+            )
           }
         })
       }
