673278c1f9cc457a7c689d393569fe83d8a17686
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-17 07:53:18 +0800

    Modify footer message

--- client/src/component/commons/Footer/Footer.js
+++ client/src/component/commons/Footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>© 2018 Your Company, Inc. All Rights Reserved.</p>
+        <p>© 2018 TIC 2601 Project, Inc. All Rights Reserved.</p>
       </footer>
     );
   }
