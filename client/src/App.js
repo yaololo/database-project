c4b820241d09ef4315ff7254966815881778a086
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-04 19:23:15 +0800

    Refactor App.js and extract the routing logic out to MyApp component

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,13 @@
-import Login from './component/commons/account/Login';
-import SignUp from "./component/commons/account/SignUp";
+
 import './App.css';
-import Header from './component/commons/Header/Header';
-import Footer from './component/commons/Footer/Footer'
-import NotFound from './component/commons/404NotFound/NotFound';
-import Home from './component/commons/Home/Home';
-import DetailedPage from './component/DetailedPage/DetailedPage';
-import ShoppingCart from './component/ShoppingCart/ShoppingCart';
-import Profile from './component/commons/account/Profile' 
-import Address from './component/Checkout/Address';
-import CheckOutConfirmation from './component/Checkout/CheckOutConfirmation';
+
 
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-contextual";
 import React, { Component } from 'react';
 import { CookiesProvider } from "react-cookie";
+import MyApp from './MyApp';
 class App extends Component {
-  isAuthenticated = false;
-
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-    };
-  }
 
   saveSession = (jwtToken, user) => {
     this.isAuthenticated = true;
@@ -41,7 +25,7 @@ class App extends Component {
 
   updateCartProductInfoList = (productInfoList) => {
     return { cartProductInfoList: productInfoList }
-  } 
+  }
 
   updateNoOfItemInCart = (noOfItems) => {
     return { noOfItems: noOfItems }
@@ -63,73 +47,18 @@ class App extends Component {
     }
   };
 
-  PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props =>
-        this.isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: props.location }
-            }}
-          />
-        )
-      }
-    />
-  );
-
   render() {
     return (
       <Provider {...this.store}>
         <CookiesProvider>
           <BrowserRouter>
-            <div>
-              <Header />
-              <Switch>
-                <Route path="/" exact render={() => <Home data={this.state.data} />} />
-                <Route path="/confirm_checkout" exact component= {CheckOutConfirmation} />
-                <this.PrivateRoute path="/account" component={Profile} />
-                <this.PrivateRoute path="/checkout/address" component={Address} />
-                <this.PrivateRoute path="/checkout/confirm_checkout" component={CheckOutConfirmation} />
-                <Route path="/login" exact component={Login} />
-                <Route path="/signup" exact component= {SignUp} />
-                <Route path="/product/:productId" exact component={DetailedPage} />
-                <this.PrivateRoute path="/my_cart" exact component={ShoppingCart} />
-                <Route path="*" component={NotFound} />
-              </Switch>
-              <div>
-                <Footer />
-              </div>
-            </div>
+            <MyApp/>
           </BrowserRouter>
         </CookiesProvider>
       </Provider>
-    );
-  }
-
-  componentWillMount() {
-    fetch('/api/hot_items', {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-      if (response.ok) {
-        response.json().then(json => {
-          this.setState({
-            data: json.data
-          });
-        });
-      } else {
-        response.json().then(json =>
-          console.log('app crash')
-        );
-      }
-    });
+    )
   }
 }
+
 
 export default App;
