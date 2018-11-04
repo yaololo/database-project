60da75ea29a4c68b6a78775dbfa09a05e391424b
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-04 15:45:34 +0800

    Reformat historical order details query result into a specific data structure

--- server/routes/actions/custOrderHist.js
+++ server/routes/actions/custOrderHist.js
@@ -8,7 +8,6 @@ const custOrderHist = async function(req, res) {
     var connection = dbSetup.connect();
     let sql =
       "call `custOrderHist`(?);";
-
     connection.query(sql, customer_id, function(
       error,
       results,
@@ -22,9 +21,43 @@ const custOrderHist = async function(req, res) {
           })
         );
       } else {
+        let map = {};
+        let orders = [];
+
+        results[0].forEach(element => {
+          if (map[element.order_id] === undefined) {
+            map[element.order_id] = [];
+            let product = {};
+            product.name = element.p_name;
+            product.Price = element.unit_price;
+            product.image = element.image;
+            product.quantity = element.quantity;
+            product.orderDate = element.order_date;
+            map[element.order_id].push(product);
+          } else {
+            let product = {};
+            product.name = element.p_name;
+            product.Price = element.unit_price;
+            product.image = element.image;
+            product.quantity = element.quantity;
+            product.orderDate = element.order_date;
+            map[element.order_id].push(product);
+          }
+        });
+
+        for (let key in map) {
+          let order = {};
+          order.id = key
+          order.details = map[key]
+          orders.push(order);
+        }
+
+        orders.sort(function(a, b) {
+          return Number(b.id) - Number(a.id);
+        });
         return res.status(200).send(
           JSON.stringify({
-            data: results
+            data: orders
           })
         );
       }
